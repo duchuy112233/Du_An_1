6041fb974e3c83be7c11e0797e0a836723a5275f
thaodvph36484 <149406775+thaodvph36484@users.noreply.github.com>
2023-11-09 02:10:15 +0700

    html

--- giaodien/home.php
+++ giaodien/home.php
@@ -20,27 +20,27 @@
     <div class="top-home">
         <div class="icon-top">
             <i class="fa-solid fa-truck-fast"></i>
-            <p>Giao Hàng Miễn Phí</p>
+            <p class="nd">Giao Hàng Miễn Phí</p>
             <p>Trên Toàn Quốc</p>
         </div>
-        <div class="icon-top">
+        <div class="icon-top icon2">
             <i class="fa-solid fa-medal"></i>
-            <p>Giao Hàng Miễn Phí</p>
-            <p>Trên Toàn Quốc</p>
+            <p class="nd">Bảo Hành Chính Hãng</p>
+            <p>Từ Laptopia Việt Nam</p>
         </div>
-        <div class="icon-top">
+        <div class="icon-top icon3">
             <i class="fa-solid fa-calendar-days"></i>
-            <p>Giao Hàng Miễn Phí</p>
-            <p>Trên Toàn Quốc</p>
+            <p class="nd">100% Bảo Mật</p>
+            <p>Thông Tin Luôn An Toàn</p>
         </div>
-        <div class="icon-top4">
+        <div class="icon-top icon4">
             <i class="fa-regular fa-thumbs-up"></i>
-            <p>Giao Hàng Miễn Phí</p>
-            <p>Trên Toàn Quốc</p>
+            <p class="nd">Thanh Toán Trực Tuyến</p>
+            <p>Credit/Debit/MoMo</p>
         </div>
     </div>
 
-    <div class="container">
+    <div class="mr-banner">
         <div class="tieude">
             <h4>SẢN PHẨM NỔI BẬT</h4>
             <hr>
@@ -49,19 +49,20 @@
             <div class="container-fuild">
                 <div class="row">
                     <!-- SP1 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="index.php?act=sanphamct"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
 
                             <div class="tensp">
-                                <a href="index.php?act=sanphamct">iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan </a>
+                                <a href="index.php?act=sanphamct">iPhone 15 Pro Max 1T Titan  </a>
                             </div>
 
                             <p class="card-price">
                                 <span> 600,000,000 VNĐ</span>
                                 <del>700,000,000 VNĐ</del>
                             </p>
+                            
 
                             <input id="them" type="submit" name="addtocart" value="Thêm vào giỏ hàng" class="themgio">
                             <i class="fa-solid fa-cart-shopping"></i>
@@ -69,7 +70,7 @@
                         </div>
                     </div>
                     <!-- SP2 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
@@ -89,13 +90,13 @@
                         </div>
                     </div>
                     <!-- SP3 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
 
                             <div class="tensp">
-                                <a href="#">iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan </a>
+                                <a href="#">iPhone 15 Pro Max 1T Titan </a>
                             </div>
 
                             <p class="card-price">
@@ -109,13 +110,13 @@
                         </div>
                     </div>
                     <!-- SP4 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
 
                             <div class="tensp">
-                                <a href="#">iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan </a>
+                                <a href="#">iPhone 15 Pro Max 1T Titan </a>
                             </div>
 
                             <p class="card-price">
@@ -129,13 +130,13 @@
                         </div>
                     </div>
                     <!-- SP5 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
 
                             <div class="tensp">
-                                <a href="#">iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan iPhone 15 Pro Max 1T Titan </a>
+                                <a href="#">iPhone 15 Pro Max 1T Titan </a>
                             </div>
 
                             <p class="card-price">
@@ -159,7 +160,7 @@
             <div class="container-fuild">
                 <div class="row">
                     <!-- SP1 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card" >
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
@@ -179,7 +180,7 @@
                         </div>
                     </div>
                     <!-- SP2 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card" >
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
@@ -199,7 +200,7 @@
                         </div>
                     </div>
                     <!-- SP3 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
@@ -219,7 +220,7 @@
                         </div>
                     </div>
                     <!-- SP4 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card" >
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
@@ -239,7 +240,7 @@
                         </div>
                     </div>
                     <!-- SP5 -->
-                    <div class="card" style="width: 18rem">
+                    <div class="card">
                         <a href="#"> <img src="image/sp1.jpg" class="imagesp" /></a>
 
                         <div class="card-body">
