3489ebdcd64e5cafcb0f4782f586493354b799ba
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2023-11-08 15:38:28 +0700

    html
    giao diện trang chủ và chi tiết sản phẩm

---
+++ giaodien/menu.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="menu">
+        <ul class="menu-row">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">TRANG CHỦ</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">DANH MỤC</a>
+                <ul class="submenu">
+                    <!-- Danh mục -->
+                    <li><a href="#">lap top</a></li>
+                    <li><a href="#">lap top</a></li>
+                    <li><a href="#">lap top</a></li>
+                    <li><a href="#">lap top</a></li>
+
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?act=lienhe">LIÊN HỆ</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?act=gopy">GÓP Ý</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?act=hoidap">HỎI ĐÁP</a>
+            </li>
+
+        </ul>
+
+      
+
+
+    </nav>
+</body>
+
+</html>
