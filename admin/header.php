e1bceda260687ee6620d4e79d3d05e1725cd0747
thaodvph36484 <149406775+thaodvph36484@users.noreply.github.com>
2023-11-10 21:04:00 +0700

    DA1

--- admin/header.php
+++ admin/header.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/header_admin.css">
-    <link rel="stylesheet" href="../css/home_admin.css">
-    <link rel="stylesheet" href="../css/footer_admin.css">
-    <link rel="stylesheet" href="../css/table_admin.css">
+    <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
 <body>
