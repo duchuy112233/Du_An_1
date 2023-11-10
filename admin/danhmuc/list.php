9ca28fe21c85374eae76207e907c5122a8575210
thaodvph36484 <149406775+thaodvph36484@users.noreply.github.com>
2023-11-10 16:26:13 +0700

    DA1

--- admin/danhmuc/list.php
+++ admin/danhmuc/list.php
@@ -3,13 +3,32 @@
 </div>
 <div class="formcontent">
     <form action="index.php?act=adddm" method="post">
-        <div class="mb10 formds_loai">
-            <table border="1" class="mb10">
+        <div class="mb10">
+            <table class="mb10 content-table">
+                <thead>
                 <tr>
                     <th>Chọn nhanh</th>
                     <th>Mã danh mục</th>
                     <th>Tên Danh Mục</th>
                     <th>Chức năng</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                        <td><input type="checkbox" name=""></td>
+                        <td>A</td>
+                        <td>A</td>
+                        <td><a href=""><input type="button" value="Sửa"></a>
+                            <a onclick="return confirm('Bạn có chắc chắn muốn xóa')" href=""><input type="button" value="Xóa"></a>
+                        </td>
+                </tr>
+                <tr class="active-row">
+                        <td><input type="checkbox" name=""></td>
+                        <td>A</td>
+                        <td>A</td>
+                        <td><a href=""><input type="button" value="Sửa"></a>
+                            <a onclick="return confirm('Bạn có chắc chắn muốn xóa')" href=""><input type="button" value="Xóa"></a>
+                        </td>
                 </tr>
                 <tr>
                         <td><input type="checkbox" name=""></td>
@@ -19,6 +38,7 @@
                             <a onclick="return confirm('Bạn có chắc chắn muốn xóa')" href=""><input type="button" value="Xóa"></a>
                         </td>
                 </tr>
+                </tbody>
             </table>
         </div>
         <input type="button" value="Chọn tất cả">
