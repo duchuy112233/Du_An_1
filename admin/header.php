3b379646af5ca7626364c20008506ea302161c2a
thaodvph36484 <149406775+thaodvph36484@users.noreply.github.com>
2023-11-10 20:01:11 +0700

    DA1

--- admin/header.php
+++ admin/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/header_admin.css">
     <link rel="stylesheet" href="../css/home_admin.css">
     <link rel="stylesheet" href="../css/footer_admin.css">
-    <!-- <link rel="stylesheet" href="../css/table_admin.css"> -->
+    <link rel="stylesheet" href="../css/table_admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
 <body>
