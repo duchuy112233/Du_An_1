d74c0bc3ba3072ea8117972143bdac01afc16450
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2023-11-13 21:11:06 +0700

    Update dangnhap.php

--- view/taikhoan/dangnhap.php
+++ view/taikhoan/dangnhap.php
@@ -1,73 +1,113 @@
-<div class="body">
+<!DOCTYPE html>
+<html lang="en">
 
-    <!-- sign in  -->
-    <!-- Dang nhap -->
-    <?php
-    if (isset($_SESSION['user'])  ) {
-        extract($_SESSION['user']);
-    ?>
-        <h2>Thông tin tài khoản</h2><br>
-        <div class="container-sign" id="container-sign">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .tttk:hover {
+            color: #009B48;
+            padding: 10px 10px;
+        }
 
-            <div style="font-size: 20px; padding: 10px 20px;">
-                Xin chào
-                <strong style="color: green;"><?= $user ?></strong> !
-                <img src="" alt="">
-            </div>
-        
-            <div style="padding: 0px 20px;">
-                <li>
-                    <a href="index.php?act=quenmk">Quên mật khẩu</a>
-                </li>
-                <li>
-                    <a href="index.php?act=edit_taikhoan">Cập nhật tài khoản</a>
-                </li>
-                <?php
-                if ($role == 1) {
-                ?>
-                    <li>
-                        <a href="../admin/index.php">Đăng nhập vào ADMIN</a>
-                    </li>
-                <?php } ?>
+        .tttk-dangxuat a {
+          font-size: 13px;
+            color: white;
 
-                <div style="padding: 20px 0px; ">
-                    <a href="index.php?act=thoat">Đăng xuất</a>
+        }
+        .mb-tk li{
+             margin-bottom: 5px; 
+        }
+        .mb-tk li a{
+            font-size: 17px;
+        }
+        .tttk-dangxuat {
+            margin-right: 70px;
+            margin-top: 200px;
+            float: right;
+        }
+
+        .tttk-dangxuat:hover {
+
+            background-color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="body">
+        <!-- Dang nhap -->
+        <?php
+        if (isset($_SESSION['user'])) {
+            extract($_SESSION['user']);
+        ?>
+            <h2>Thông tin tài khoản</h2><br>
+            <div class="container-sign" id="container-sign">
+
+                <div style="font-size: 20px; padding: 30px 50px;">
+                    Xin chào
+                    <strong style="color: green;"><?= $user ?></strong> !
+                    <img src="" alt="">
                 </div>
 
-            </div>
-        </div>
+                <div class="mb-tk" style="padding: 0px 30px;">
+                    <li>
+                        <a class="tttk" href="index.php?act=quenmk">Quên mật khẩu</a>
+                    </li>
+                    <li>
+                        <a class="tttk" href="index.php?act=edit_taikhoan">Cập nhật tài khoản</a>
+                    </li>
+                    <?php
+                    if ($role == 1) {
+                    ?>
+                        <li>
+                            <a class="tttk" href="../admin/index.php">Đăng nhập vào ADMIN</a>
+                        </li>
+                    <?php } ?>
 
-    <?php
-    } else {
-    ?>
-        <div class="">
-            <h2>Đăng nhập</h2><br>
-            <div class="container-sign" id="container-sign">
-                <form action="index.php?act=dangnhap" method="post">
+                    <div style="padding: 20px 0px; ">
+                        <button class="tttk-dangxuat"><a href="index.php?act=thoat">Đăng xuất</a></button>
 
-                    <div class="social-container">
-                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
                     </div>
-                    <span>Sử dụng phương thức đăng nhập khác</span>
-                    <input name="email" type="text" placeholder="Email" />
-                    <input name="pass" type="password" placeholder="Password" />
-                    <div style="color: red; font-size: 12px; font-weight: 500;">
-                        <?php
-                        if (isset($thongbao) && ($thongbao != "")) {
-                            echo $thongbao;
-                        }
-                        ?>
-                    </div>
-                    <br>
-                    <a href="#">Quên mật khẩu</a>
-                    <p>Bạn chưa có tài khoản? <a href="index.php?act=dk"><span class="dk">Đăng kí ngay</span></a></p>
-                    <br>
-                    <button type="submit" name="dangnhap" value="Đăng nhập">Đăng nhập</button>
-                </form>
+
+                </div>
             </div>
-        </div>
-    <?php } ?>
-</div>
-</div>
+
+        <?php
+        } else {
+        ?>
+            <div class="">
+                <h2>Đăng nhập</h2><br>
+                <div class="container-sign" id="container-sign">
+                    <form action="index.php?act=dangnhap" method="post">
+
+                        <div class="social-container">
+                            <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>Sử dụng phương thức đăng nhập khác</span>
+                        <input name="email" type="text" placeholder="Email" />
+                        <input name="pass" type="password" placeholder="Password" />
+                        <div style="color: red; font-size: 12px; font-weight: 500;">
+                            <?php
+                            if (isset($thongbao) && ($thongbao != "")) {
+                                echo $thongbao;
+                            }
+                            ?>
+                        </div>
+                        <br>
+                        <a href="#">Quên mật khẩu</a>
+                        <p>Bạn chưa có tài khoản? <a href="index.php?act=dk"><span class="dk">Đăng kí ngay</span></a></p>
+                        <br>
+                        <button type="submit" name="dangnhap" value="Đăng nhập">Đăng nhập</button>
+                    </form>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    </div>
+</body>
+
+</html>
