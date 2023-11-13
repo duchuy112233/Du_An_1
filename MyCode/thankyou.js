e3b0483915130536f92cc9385f08123218920060
NguyenVanNhan-09 <nhan09122004@gmail.com>
2023-11-14 00:32:50 +0700

    up

---
+++ MyCode/thankyou.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('.title');
+title.addEventListener('mouseover', function() {
+  title.style.color = '#ff758c';
+});
+title.addEventListener('mouseout', function() {
+  title.style.color = '#ff4e7e';
+});
