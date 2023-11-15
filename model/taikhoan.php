3ddd67a0a648645401e639823119005f12b603b3
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2023-11-15 07:21:15 +0700

    Update taikhoan.php

--- model/taikhoan.php
+++ model/taikhoan.php
@@ -62,3 +62,52 @@ function loadone_taikhoan($id)
     return $sp;
 }
 
+function sendMailPass($email, $username, $pass) {
+    require '../PHPMailer/src/Exception.php';
+    require '../PHPMailer/src/PHPMailer.php';
+    require '../PHPMailer/src/SMTP.php';
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = PHPMailer\PHPMailer\SMTP::DEBUG_OFF;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'sandbox.smtp.mailtrap.io';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = '00a2d4229bd65b';                     //SMTP username
+    $mail->Password   = '6dd329c899a82a';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('shoplaptopia@example.com', 'Shop Laptopia');
+    $mail->addAddress($email, $username);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->CharSet = 'UTF-8';                             // Set character encoding to UTF-8
+    $mail->Encoding = 'base64';                           // Set the encoding type to base64 (optional, but recommended)
+    $mail->Subject = 'Mật khẩu mới';
+    $mail->Body    = 'Mật khẩu mới của bạn là: '.$pass;
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    // echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+//Đổi mật khẩu của người dùng
+function update_matkhau($id,$pass){
+    $sql="UPDATE tai_khoan SET pass='$pass' where id= '$id'";
+    pdo_execute($sql);
+}
