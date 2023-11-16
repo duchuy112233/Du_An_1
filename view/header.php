726b3ba70ccd32eb334f4e1632f3f12a7a10069b
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2023-11-16 09:20:39 +0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <link rel="stylesheet" href="../css/view.css">
 </head>
+
 <body>
     <div class="container">
         <div class="top-header"></div>
@@ -19,25 +21,26 @@
                 </form>
                 <div class="form-menu">
                     <div class="dn-header">
-                        <?php if(isset($_SESSION['user'])) { ?>
+                        <?php if (isset($_SESSION['user'])) { ?>
                             <li class="nav-item"><a href="index.php?act=dangnhap"><i class="fa-solid fa-user" style="color: #000000;"></i> <?php echo $_SESSION['user']['user'] ?> </i></a>
-                            <ul class = "submenu">
-                                <li class="header-tk"> <a  href="index.php?act=dangnhap">Thông tin tài khoản </a></li>
-                           
-                                <li class="header-tk"> <a  href="index.php?act=doimk">Đổi mật khẩu </a></li>
-                                <?php if($_SESSION['user']['role']==1){ ?>
-                                <li class="header-tk"> <a  href="../admin/index.php">Đăng nhập vào ADMIN</a></li>
-                                <?php } ?>
-                                <li class="header-tk"> <a  href="index.php?act=thoat">Đăng xuất</a></li>
-                            </ul></li>
+                                <ul class="submenu">
+                                    <li class="header-tk"> <a href="index.php?act=dangnhap">Thông tin tài khoản </a></li>
+
+                                    <li class="header-tk"> <a href="index.php?act=doimk">Đổi mật khẩu </a></li>
+                                    <?php if ($_SESSION['user']['role'] == 1) { ?>
+                                        <li class="header-tk"> <a href="../admin/index.php">Đăng nhập vào ADMIN</a></li>
+                                    <?php } ?>
+                                    <li class="header-tk"> <a href="index.php?act=thoat">Đăng xuất</a></li>
+                                </ul>
+                            </li>
                         <?php } else { ?>
                             <li class="nav-item"><a href="index.php?act=dangnhap"><i class="fa-solid fa-user" style="color: #000000;"></i> Đăng nhập </i></a>
-                            <ul class = "submenu">
-                                <li class="header-tk"> <a  href="index.php?act=dangky">Đăng ký </a></li>
-                                <li class="header-tk"> <a  href="index.php?act=quenmk">Quên mật khẩu </a></li>
-                            </ul>
+                                <ul class="submenu">
+                                    <li class="header-tk"> <a href="index.php?act=dangky">Đăng ký </a></li>
+                                    <li class="header-tk"> <a href="index.php?act=quenmk">Quên mật khẩu </a></li>
+                                </ul>
                             </li>
-                        <?php } ?> 
+                        <?php } ?>
                     </div>
                     <div class="giohang-icon">
                         <a href="index.php?act=addtocart"> <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i> Giỏ hàng </a>
@@ -47,7 +50,7 @@
         </div>
 
 
-        
+
 
         <div class="menu">
             <ul class="menu-row">
@@ -79,3 +82,5 @@
                 </li>
             </ul>
         </div>
+    </div>
+</body>
