efa6f9001a90cd08fd29191ef3a8b95b25ded1a0
thaodvph36484 <149406775+thaodvph36484@users.noreply.github.com>
2023-11-25 14:03:38 +0700

    giohang and dathang

---
+++ view/cart/bill.php
@@ -0,0 +1,76 @@
+<div class="bill">
+    <div class="left">
+        <form action="index.php?act=billcomfirm" method="post">
+            <div class="order-form">
+            <p>Thông tin người đặt hàng</p>
+                <?php
+                if (isset($_SESSION['user'])) {
+                    $name = $_SESSION['user']['user'];
+                    $address = $_SESSION['user']['address'];
+                    $email = $_SESSION['user']['email'];
+                    $tel = $_SESSION['user']['tel'];
+                } else {
+                    $name = "";
+                    $address = "";
+                    $email = "";
+                    $tel = "";
+                }
+                ?>
+                <div>
+                    <label>Người đặt hàng: </label>
+                    <input type="text" name="name" value="<?php echo $name ?>" required>
+                </div><br>
+                <div>
+                    <label>Địa chỉ:</label>
+                    <input type="text" name="address" value="<?php echo $address ?>" required>
+                </div><br>
+                <div>
+                    <label>Email:</label>
+                    <input type="text" name="email" value="<?php echo $email ?>" required>
+                </div><br>
+                <div>
+                    <label>Số điện thoại:</label>
+                    <input type="text" name="tel" value="<?php echo $tel ?>" required>
+                </div>
+            </div>
+            <div class="payment">
+                <p>Phương thức thanh toán</p>
+                <input type="radio" value="1" name="pttt" id="payment1" checked>
+                <label for="payment1" style="margin-right: 30px;">Trả tiền khi nhận hàng</label>
+                <input type="radio" value="2" name="pttt" id="payment2">
+                <label for="payment2">Thanh toán online</label>
+            </div><br>
+            <a href="index.php?act=billcomfirm"><input type="submit" name="dathang" value="Đồng ý đặt hàng"></a>
+        </form>
+    </div>
+
+
+    <div class="giohang">
+        <p class="title-cart">Tóm tắt đơn hàng</p>
+        <?php $tong = 0;
+        foreach ($_SESSION['mycart'] as $cart) {
+            $hinh = $image_path . $cart[2];
+            $tongtien = intval($cart[3]) * intval($cart[4]);
+            $tong += $tongtien; ?>
+            <div class="order">
+                <img src="<?php echo $hinh ?>" alt="" width="120px" height="120px" style="border:1px solid #ccc">
+                <div>
+                    <div class="content-on">
+                        <p><?php echo $cart[1] ?></p>
+                        <span><?php echo $cart[5] ?></span>
+                        <span><?php echo $cart[6] ?></span>
+                    </div>
+                    <div class="content-below">
+                        <p>Đơn giá: <?php echo number_format($cart[3]) ?> VND</p>
+                        <p>Số lượng: <?php echo $cart[4] ?></p>
+                        <p>Thành tiền: <?php echo number_format($tongtien) ?> VND</p>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+        <div>
+            <p class="total">Tổng đơn hàng: <?php echo number_format($tong) ?> VND</p>
+        </div>
+    </div>
+
+</div>
--- view/index.php
+++ view/index.php
@@ -10,11 +10,11 @@ include "../model/bienthe.php";
 include "../model/cart.php";
 include "../global.php";
 
-$listdm=loadall_danhmuc();
-$dm1=loadall_sanpham_dm1();
-$dm2=loadall_sanpham_dm2();
-if(!isset($_SESSION['mycart'])){
-   $_SESSION['mycart']=[];
+$listdm = loadall_danhmuc();
+$dm1 = loadall_sanpham_dm1();
+$dm2 = loadall_sanpham_dm2();
+if (!isset($_SESSION['mycart'])) {
+    $_SESSION['mycart'] = [];
 }
 
 include "header.php";
@@ -23,118 +23,110 @@ if (isset($_GET['act']) && ($_GET['act']) != "") {
     $act = ($_GET['act']);
     switch ($act) {
         case "danhmucsp":
-            if(isset($_POST['keyword']) &&  $_POST['keyword'] != 0 ){
-            $kyw = $_POST['keyword'];
-            }else{
-            $kyw = "";
+            if (isset($_POST['keyword']) &&  $_POST['keyword'] != 0) {
+                $kyw = $_POST['keyword'];
+            } else {
+                $kyw = "";
             }
-            if(isset($_GET['iddm']) && $_GET['iddm']>0){
-            $iddm=$_GET['iddm'];
-            }else{
-            $iddm=0;
+            if (isset($_GET['iddm']) && $_GET['iddm'] > 0) {
+                $iddm = $_GET['iddm'];
+            } else {
+                $iddm = 0;
             }
-            $dssp=loadall_sanpham($kyw,$iddm);
+            $dssp = loadall_sanpham($kyw, $iddm);
             include "danhmuc.php";
             break;
         case 'sanphamct':
-            if(isset($_POST['guibinhluan'])){
-                if(empty($_POST['noidung'])){
-                $thongbao8="Vui lòng nhập nội dung bình luận!";
-                }
-                else{
-                    $iduser=$_SESSION['user']['id'];
-                    insert_binhluan($_POST['idsp'], $_POST['noidung'],$iduser);
+            if (isset($_POST['guibinhluan'])) {
+                if (empty($_POST['noidung'])) {
+                    $thongbao8 = "Vui lòng nhập nội dung bình luận!";
+                } else {
+                    $iduser = $_SESSION['user']['id'];
+                    insert_binhluan($_POST['idsp'], $_POST['noidung'], $iduser);
                 }
             }
-            if(isset($_GET['idsp']) && $_GET['idsp'] > 0 ){
+            if (isset($_GET['idsp']) && $_GET['idsp'] > 0) {
                 tangluotxem($_GET['idsp']);
-                $onesp=loadone_sanpham($_GET['idsp']);
-                $onebtram=load_btram($_GET['idsp']);
-                $sp_cungloai=load_sanpham_cungloai($_GET['idsp'],$onesp['iddm']);
-                $binhluan=load_binhluan($_GET['idsp']);
+                $onesp = loadone_sanpham($_GET['idsp']);
+                $onebtram = load_btram($_GET['idsp']);
+                $sp_cungloai = load_sanpham_cungloai($_GET['idsp'], $onesp['iddm']);
+                $binhluan = load_binhluan($_GET['idsp']);
             }
             include "sanphamct.php";
             break;
         case 'dangky':
-            if(isset($_POST['dangky'])){
-                if(empty($_POST['email']) || empty($_POST['user']) || empty($_POST['pass'])){
-                    $thongbao2="Vui lòng nhập đầy đủ!";
-                }
-                else{
-                    $email=$_POST['email'];
-                    $user=$_POST['user'];
-                    $pass=$_POST['pass'];
-                    add_taikhoan($email,$user,$pass);
-                    $thongbao2="Đăng kí thành công";
+            if (isset($_POST['dangky'])) {
+                if (empty($_POST['email']) || empty($_POST['user']) || empty($_POST['pass'])) {
+                    $thongbao2 = "Vui lòng nhập đầy đủ!";
+                } else {
+                    $email = $_POST['email'];
+                    $user = $_POST['user'];
+                    $pass = $_POST['pass'];
+                    add_taikhoan($email, $user, $pass);
+                    $thongbao2 = "Đăng kí thành công";
                 }
             }
             include "taikhoan/dangky.php";
             break;
         case 'dangnhap':
-            if(isset($_POST['dangnhap'])){
-                $email=$_POST['email'];
-                $pass=$_POST['pass'];
-                $login=dangnhap($email,$pass);
-                if(is_array($login)){
-                $_SESSION['user']=$login;
-                header("location: index.php");
+            if (isset($_POST['dangnhap'])) {
+                $email = $_POST['email'];
+                $pass = $_POST['pass'];
+                $login = dangnhap($email, $pass);
+                if (is_array($login)) {
+                    $_SESSION['user'] = $login;
+                    header("location: index.php");
+                } else if (empty($_POST['email']) || empty($_POST['pass'])) {
+                    $thongbao3 = "Vui lòng nhập đầy đủ!";
+                } else {
+                    $thongbao3 = "Tài khoản hoặc mật khẩu sai!";
                 }
-                else if(empty($_POST['email']) || empty($_POST['pass'])){
-                $thongbao3="Vui lòng nhập đầy đủ!";
-                }
-                else{
-                $thongbao3= "Tài khoản hoặc mật khẩu sai!";
-                }
-                }
-                include "taikhoan/dangnhap.php";
+            }
+            include "taikhoan/dangnhap.php";
             break;
         case 'taikhoan':
-            if(isset($_POST['capnhat'])){
-                if(empty($_POST['user'])){
-                    $thongbao7="Tên đăng nhập không được để trống!";
-                }
-                else{
-                    $user=$_POST['user'];
-                    $email=$_POST['email'];
-                    $pass=$_POST['pass'];
-                    $address=$_POST['address'];
-                    $tel=$_POST['tel'];
-                    $id=$_POST['id'];
-                    $img=$_FILES['img']['name'];
-                    $target_file=$image_path.time().basename($_FILES['img']['name']);
-                    move_uploaded_file($_FILES['img']['tmp_name'],$target_file);
-                    edit_taikhoan($id,$user,$img,$address,$tel);
-                    $_SESSION['user']=dangnhap($email,$pass);
+            if (isset($_POST['capnhat'])) {
+                if (empty($_POST['user'])) {
+                    $thongbao7 = "Tên đăng nhập không được để trống!";
+                } else {
+                    $user = $_POST['user'];
+                    $email = $_POST['email'];
+                    $pass = $_POST['pass'];
+                    $address = $_POST['address'];
+                    $tel = $_POST['tel'];
+                    $id = $_POST['id'];
+                    $img = $_FILES['img']['name'];
+                    $target_file = $image_path . time() . basename($_FILES['img']['name']);
+                    move_uploaded_file($_FILES['img']['tmp_name'], $target_file);
+                    edit_taikhoan($id, $user, $img, $address, $tel);
+                    $_SESSION['user'] = dangnhap($email, $pass);
                     // $thongbao7="Cập nhật thành công. Xin hãy đăng nhập lại!";
                 }
             }
             include "taikhoan/taikhoan.php";
             break;
         case 'doimk':
-            if(isset($_POST['doimk'])){
-                $pass_csdl=$_SESSION['user']['pass'];
-                if(empty($_POST['pass']) || empty($_POST['newpass']) || empty($_POST['repass'])){
-                $thongbao4="Vui lòng nhập đầy đủ!";
+            if (isset($_POST['doimk'])) {
+                $pass_csdl = $_SESSION['user']['pass'];
+                if (empty($_POST['pass']) || empty($_POST['newpass']) || empty($_POST['repass'])) {
+                    $thongbao4 = "Vui lòng nhập đầy đủ!";
+                } else if ($_POST['pass'] != $pass_csdl) {
+                    $thongbao4 = "Mật khẩu cũ không chính xác!";
+                } else if ($_POST['repass'] != $_POST['newpass']) {
+                    $thongbao4 = "Mật khẩu mới không trùng khớp!";
+                } else {
+                    $newpass = $_POST['newpass'];
+                    $idtk = $_POST['idtk'];
+                    update_matkhau($idtk, $newpass);
+                    $thongbao4 = "Đổi mật khẩu thành công";
                 }
-                else if($_POST['pass'] != $pass_csdl){
-                    $thongbao4="Mật khẩu cũ không chính xác!";
-                }
-                else if($_POST['repass'] != $_POST['newpass']){
-                    $thongbao4="Mật khẩu mới không trùng khớp!";
-                }
-                else{
-                    $newpass=$_POST['newpass'];
-                    $idtk=$_POST['idtk'];
-                    update_matkhau($idtk,$newpass);
-                    $thongbao4="Đổi mật khẩu thành công";
-                }
-                }
+            }
             include "taikhoan/doimk.php";
             break;
         case 'quenmk':
-            if(isset($_POST['quenmk'])){
-                $email=$_POST['email'];
-                $sendMail=sendMail($email);
+            if (isset($_POST['quenmk'])) {
+                $email = $_POST['email'];
+                $sendMail = sendMail($email);
             }
             include "taikhoan/quenmk.php";
             break;
@@ -143,74 +135,96 @@ if (isset($_GET['act']) && ($_GET['act']) != "") {
             header("location: index.php");
             break;
         case 'addtocart':
-            if(isset($_POST['addtocart'])){
-                $id=$_POST['id'];
-                $name=$_POST['name'];
-                $hinh=$_POST['hinh'];
-                $price=$_POST['price'];
-                if(isset($_POST['soluong'])){
-                $soluong=$_POST['soluong'];
+            if (isset($_POST['addtocart'])) {
+                $id = $_POST['id'];
+                $name = $_POST['name'];
+                $hinh = $_POST['hinh'];
+                $price = $_POST['price'];
+                if (isset($_POST['soluong'])) {
+                    $soluong = $_POST['soluong'];
+                } else {
+                    $soluong = 1;
                 }
-                else{
-                    $soluong=1;
-                }
-                if(isset($_POST['idram']) && !empty($_POST['idram'])){
-                $oneram=loadone_ram($_POST['idram']);
-                $ram=$oneram['ram_sp'];
-                }
-                else{
-                    $ram="";
+                $tongtien=$soluong*$price;
+                if (isset($_POST['idram']) && !empty($_POST['idram'])) {
+                    $oneram = loadone_ram($_POST['idram']);
+                    $ram = $oneram['ram_sp'];
+                } else {
+                    $ram = "";
                 }
 
-                if(isset($_POST['idmau']) && !empty($_POST['idmau'])){
-                $onemau=loadone_mau($_POST['idmau']);
-                $mau=$onemau['mau_sp'];
+                if (isset($_POST['idmau']) && !empty($_POST['idmau'])) {
+                    $onemau = loadone_mau($_POST['idmau']);
+                    $mau = $onemau['mau_sp'];
+                } else {
+                    $mau = "";
                 }
-                else{
-                    $mau="";
-                }
-                $fg=0;
-                $i=0;
+                $fg = 0;
+                $i = 0;
                 foreach ($_SESSION['mycart'] as $item) {
-                    if($item[1]==$name&& $item[5] == $ram && $item[6] == $mau){
-                        $slnew=$soluong+$item[4];
-                        $_SESSION['mycart'][$i][4]=$slnew;
-                        $fg=1;
+                    if ($item[1] == $name && $item[5] == $ram && $item[6] == $mau) {
+                        $slnew = $soluong + $item[4];
+                        $_SESSION['mycart'][$i][4] = $slnew;
+                        $fg = 1;
                         break;
                     }
                     $i++;
                 }
-                if($fg==0){
-                    $spadd=[$id,$name,$hinh,$price,$soluong,$ram,$mau];
-                    array_push($_SESSION['mycart'],$spadd);
+                if ($fg == 0) {
+                    $spadd = [$id, $name, $hinh, $price, $soluong, $ram, $mau, $tongtien];
+                    array_push($_SESSION['mycart'], $spadd);
                 }
             }
             include "cart/viewcart.php";
             break;
         case 'deletecart':
-            if(isset($_GET['idcart'])){
-                array_splice($_SESSION['mycart'],$_GET['idcart'],1);
+            if (isset($_GET['idcart'])) {
+                array_splice($_SESSION['mycart'], $_GET['idcart'], 1);
+            } else {
+                $_SESSION['mycart'] = [];
             }
-            else{
-                $_SESSION['mycart']=[];
-            }
-                header("location: index.php?act=viewcart");
+            header("location: index.php?act=viewcart");
             break;
         case 'viewcart':
             include "cart/viewcart.php";
             break;
         case 'bill':
+
+            include "cart/bill.php";
+            break;
+        case 'billcomfirm':
+            if (isset($_SESSION['user'])) {
+                $iduser = $_SESSION['user']['id'];
+            }
+            //tạo bill
+            if (isset($_POST['dathang'])) {
+                $name = $_POST['name'];
+                $address = $_POST['address'];
+                $email = $_POST['email'];
+                $tel = $_POST['tel'];
+                $pttt = $_POST['pttt'];
+                $ngaydh = date('Y-m-d');
+                $tongdonhang = tongdonhang();
+                $idbill = add_bill($iduser, $name, $address, $tel, $email, $pttt, $ngaydh, $tongdonhang);
+                //insert into cart: $_SESSION['mycart'] & idbill
+                foreach ($_SESSION['mycart'] as $cart) {
+                    add_cart($_SESSION['user']['id'], $cart[0], $cart[2], $cart[1], $cart[5], $cart[6], $cart[3], $cart[4], $cart[7], $idbill);
+                    //xóa $_SESSION['cart']
+                    $_SESSION['mycart'] = [];
+                }
+            }
+            $bill = loadone_bill($idbill);
+            $billcart = loadall_cart($idbill);
+            include "cart/billcomfirm.php";
             break;
         case 'mybill':
             break;
         default:
             include "home.php";
             break;
-        }
-    } 
-    else {
-        include "home.php";
     }
+} else {
+    include "home.php";
+}
 
 include "footer.php";
-?>
